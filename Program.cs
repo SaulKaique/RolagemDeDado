68a89812f869bcea198ce6b1f4cc1aaca80e46b8
SaulKaique <Saul.silva@etec.sp.gov.br>
2022-10-25 13:36:55 -0300

    Ja terminado com algumas diferenças do original

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("--- Rolagem de Dado ---\n");
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Quantas faces? ");
+int faces = Convert.ToInt32(Console.ReadLine());
+
+
+
+int Rolagem = new Random().Next(1,100);
+Console.ResetColor();
+
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"\nRolagem: {Rolagem}");
+Console.ResetColor();
